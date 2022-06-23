c4b1847301e1143543624616e780980dd6f46a6c
Angelica <Angelica137@users.noreply.github.com>
2022-06-23 20:10:11 +0100

    isogram(eleven) returns false, green

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -1,6 +1,9 @@
 def isogram(word: str) -> bool:
     for i in range(len(word) - 1):
         j = i + 1
-        if word[i] == word[j] and word[i] != " " and word[i] != "-":
-            return False
+        while j <= len(word) - 1:
+            if word[i] == word[j] and word[i] != " " and word[i] != "-":
+                return False
+            else:
+                j += 1
     return True
