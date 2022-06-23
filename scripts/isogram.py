bb522b5949afdfc17b1583c81c6f8e88e54fbb73
Angelica <Angelica137@users.noreply.github.com>
2022-06-23 19:55:50 +0100

    isaogram() returns False, green

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -1,2 +1,6 @@
 def isogram(word: str) -> bool:
+    for i in range(len(word) - 1):
+        j = i + 1
+        if word[i] == word[j] and word[i] != " " and word[i] != "-":
+            return False
     return True
