93c2243647056c53c7f245c8256d648d37ccfa41
Angelica <Angelica137@users.noreply.github.com>
2022-06-25 19:45:23 +0100

    all tests pass

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -1,5 +1,6 @@
 def isogram(word: str) -> bool:
-    for i in range(len(word) - 1):
+    chars = {}
+    for i in range(len(word)):
         j = i + 1
         while j <= len(word) - 1:
             if word[i] == word[j] and word[i] != " " and word[i] != "-":
