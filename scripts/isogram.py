1ef1fe72753bb4bd8da7e005783fa65fe43cfb12
Angelica <Angelica137@users.noreply.github.com>
2022-06-25 20:12:00 +0100

    remove print statement

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -2,7 +2,6 @@ def isogram(word: str) -> bool:
     chars = {}
     for i in range(len(word)):
         if word[i] in chars and word[i] != " " and word[i] != "-":
-            print(i)
             return False
         else:
             chars[word[i].lower()] = 1
