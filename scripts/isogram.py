d93b71c360d65b2a8958942495909fd5e8aeb4e6
Angelica <Angelica137@users.noreply.github.com>
2022-06-25 20:15:57 +0100

    always make th eletters little, all tests pass

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -1,8 +1,12 @@
 def isogram(word: str) -> bool:
     chars = {}
     for i in range(len(word)):
-        if word[i] in chars and word[i] != " " and word[i] != "-":
+        if word[i].lower() in chars and word[i] != " " and word[i] != "-":
             return False
         else:
             chars[word[i].lower()] = 1
+    print(chars)
     return True
+
+
+print(isogram("alphAbet"))
