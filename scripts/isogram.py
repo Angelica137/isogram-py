dd83962527c820226a119c58580a84de68bfa9bd
Angelica <Angelica137@users.noreply.github.com>
2022-06-25 20:05:34 +0100

    delete obsolete and print statements

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -7,14 +7,3 @@ def isogram(word: str) -> bool:
         else:
             chars[word[i]] = 1
     return True
-    for i in range(len(word)):
-        j = i + 1
-        while j <= len(word) - 1:
-            if word[i] == word[j] and word[i] != " " and word[i] != "-":
-                return False
-            else:
-                j += 1
-    return True
-
-
-print(isogram("eleven"))
