8615bb3fab2ed3cb125afede1bddccd568a2f519
Angelica <Angelica137@users.noreply.github.com>
2022-06-25 20:05:07 +0100

    refactor for performance

--- scripts/isogram.py
+++ scripts/isogram.py
@@ -1,6 +1,13 @@
 def isogram(word: str) -> bool:
     chars = {}
     for i in range(len(word)):
+        if word[i] in chars and word[i] != " " and word[i] != "-":
+            print(i)
+            return False
+        else:
+            chars[word[i]] = 1
+    return True
+    for i in range(len(word)):
         j = i + 1
         while j <= len(word) - 1:
             if word[i] == word[j] and word[i] != " " and word[i] != "-":
@@ -8,3 +15,6 @@ def isogram(word: str) -> bool:
             else:
                 j += 1
     return True
+
+
+print(isogram("eleven"))
